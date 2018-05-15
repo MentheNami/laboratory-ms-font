e5608465ef0588b7bb73d3e67d9c64c96120df42
pipi <619202235@qq.com>
2018-05-15 19:27:59 +0800

    实验室预定

--- src/model/laboratory/DoomLaboratory.vue
+++ src/model/laboratory/DoomLaboratory.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div>
-      <span>选择实验室</span>
+      <span style="float: left;margin-left: 40px">选择实验室</span>
       <select-customize-option :customizeOption="laboratoryList" v-model="laboratoryId"
-                               style="float:left;width: 200px;margin: 0 0 0 5px"></select-customize-option>
+                               style="float:left;width: 200px;margin: 0 0 0 10px"></select-customize-option>
     </div>
-    <div>
-      <el-tabs tab-position="left" value="todayName" style="float: left; height: 500px;" @tab-click="changeTab">
+    <div style="clear: both">
+      <el-tabs tab-position="left" value="todayName" style="float: left; height: 340px;" @tab-click="changeTab">
         <el-tab-pane :label="transferToday(today)" name="todayName"></el-tab-pane>
         <el-tab-pane :label="transferToday(today + 1)"></el-tab-pane>
         <el-tab-pane :label="transferToday(today + 2)"></el-tab-pane>
@@ -15,19 +15,27 @@
         <el-tab-pane :label="transferToday(today + 5)"></el-tab-pane>
         <el-tab-pane :label="transferToday(today + 6)"></el-tab-pane>
       </el-tabs>
-      <ul>
-        <li v-for="item in items" :key="item.id">
+      <ul class="weekul">
+        <li v-for="item in items" :key="item.id" style="margin: 0; padding:0; height: 39px; width: 450px;">
           <el-button :disabled="item.isDisable" :style="item.style" plain @click="clicks(item.id)">{{item.id + 9}}:00 -
             {{item.id + 10}}:00
           </el-button>
         </li>
       </ul>
-      <span>申请说明</span>
-      <el-input v-model="laboratoryScheduleForm.instruction" placeholder="请输入申请说明"></el-input>
+      <div style="float: left;margin: 0;padding: 0">
+      <p>
+        <b>按钮颜色状态说明:</b>
+        <el-tag style="background-color: #AFEEEE;margin-left: 10px;width: 100px">可预定</el-tag>
+        <el-tag style="background-color: #A9A9A9;color: black;width: 100px">已预定</el-tag>
+        <el-tag style="background-color: #FFA07A;color:	#FF6347;width: 100px ">正在选择</el-tag>
+      </p>
+      <b style="float: left">申请说明:</b>
+      <el-input v-model="laboratoryScheduleForm.instruction" placeholder="请输入申请说明" style="float: left; width: 310px;margin-left: 10px"></el-input>
       <el-tooltip content="添加" placement="bottom" effect="light">
         <el-button type="success" icon="el-icon-check" circle @click="submitForm"
-                   style="clear:both;float: left"></el-button>
+                   style="float: left; margin: 10px 0 0 30px"></el-button>
       </el-tooltip>
+      </div>
     </div>
   </div>
 </template>
@@ -83,13 +91,13 @@
 
         // 四种颜色
         // 可以预定
-        notScheduled: 'background-color: #7FFF00',
+        notScheduled: 'background-color: #AFEEEE;width:300px;',
         // 等待审批
-        pendingScheduled: 'background-color: #8B7E66',
+        pendingScheduled: 'background-color: #A9A9A9;width:300px;',
         // 已经预定
-        bookingScheduled: 'background-color: #FF7F00',
+        bookingScheduled: 'background-color: #A9A9A9;width:300px;',
         // 正在选择
-        selectScheduled: 'background-color: #20B2AA',
+        selectScheduled: 'background-color: #FFA07A;width:300px;',
 
         selectScheduledIndex: [],
 
@@ -97,49 +105,49 @@
           {
             id: 0,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 1,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 2,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 3,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 4,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 5,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 6,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
           {
             id: 7,
             index: '',
-            style: 'background-color: #7FFF00',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
             isDisable: false
           },
         ],
@@ -151,6 +159,63 @@
     },
 
     methods: {
+
+      resetScheduled() {
+        let self = this;
+        // 重置
+        self.items = [
+          {
+            id: 0,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 1,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 2,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 3,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 4,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 5,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 6,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+          {
+            id: 7,
+            index: '',
+            style: 'background-color: #AFEEEE;width:300px;border: none',
+            isDisable: false
+          },
+        ];
+        self.pressScheduled = -1;
+        self.againScheduled = -1;
+      },
 
       async getLaboratoryList() {
         let self = this;
@@ -217,7 +282,7 @@
       },
 
       transferToday(val) {
-        switch (val) {
+        switch (val % 7) {
           case 0:
             return "星期日";
           case 1:
@@ -238,11 +303,13 @@
       changeTab(value) {
         let self = this;
         self.searchForm.offset = value.index;
+        self.getScheduleList();
       },
 
       async getScheduleList() {
         let self = this;
         let result = await laboratoryScheduleAPI.getLaboratoryScheduleList(self.searchForm);
+        self.resetScheduled();
         // 封装数据
         if (result.status) {
           for (let laboratorySchedule of result.records) {
@@ -327,5 +394,8 @@
 </script>
 
 <style scoped>
-
+.weekul{
+  list-style-type: none;
+  float: left;
+}
 </style>
